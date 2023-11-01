405c6e5d89da38f3e261c2133c8cb7d715031a39
Satyam Gangwar <satyamgangwar2002@gmail.com>
2023-11-01 23:19:04 +0530

    HashMap Code Added

---
+++ src/HashMap.java
@@ -0,0 +1,60 @@
+import java.util.LinkedList;
+
+public class HashMap<K, V> {
+
+        private static class Node<K,V> {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private LinkedList<Node<K, V>>[] table;
+    private int size;
+
+    public HashMap() {
+        table = new LinkedList[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public void put(K key, V value){
+        int index = getHash(key);
+
+        if(table[index] == null){
+            table[index] = new LinkedList<>();
+        }
+
+        for( Node<K,V> node : table[index]){
+            if(node.key.equals(key)){
+                node.value = value;
+                return;
+            }
+        }
+
+        table[index].add(new Node<>(key, value));
+        size++;
+    }
+
+    public V get(K key){
+        int index = getHash(key);
+
+        if (table[index] != null) {
+            for (Node<K, V> node : table[index]) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                }
+            }
+        }
+        return null;
+    }
+
+    private int getHash(K key){
+        int hash = key.hashCode();
+        return hash % table.length;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String,Integer>  map = new HashMap();
+        map.put("India",1);
+        map.put("Nepal",2);
+        System.out.println(map.get("Nepal"));
+    }
+}
